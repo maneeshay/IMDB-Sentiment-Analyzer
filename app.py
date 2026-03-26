18096a740a67130c8af2746462aca37d7a41a587
Manisha Yadav <mansa.yadav05@gmail.com>
2026-03-25 21:52:02 -0400

    swapped th UI framework from streamlit to Gradio

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import streamlit as st
-import re
+import gradio as gr
 import pickle
+import re
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
-
-# Load saved model and vectorizer
-model = pickle.load(open('model.pkl', 'rb'))
+#load saved model and vectorizer
+model = pickle.load(open('model.pkl','rb'))
 tfidf = pickle.load(open('tfidf.pkl','rb'))
 
-# Clean text function
+#clean text function
 def clean_text(text):
-    text = re.sub(r'<.*?>', '', text)
+    text = re.sub(r'<.*?>' ,'', text)
     text = re.sub(r'http\S+|www\s+', '', text)
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     text = text.lower()
@@ -21,24 +20,21 @@ def clean_text(text):
     words = [word for word in words if word not in stop_words]
     return ' '.join(words)
 
-# App title
-st.title("IMDB Sentiment Analyzer")
-st.write("Type a movie review and find out if it's positive or negative! ")
+# Prediction function
+def predict_sentiment(review):
+    cleaned = clean_text(review)
+    vectorized = tfidf.transform([cleaned])
+    prediction = model.predict(vectorized)[0]
+    confidence = max(model.predict_proba(vectorized)[0]) *100
+    return f"{prediction.upper()} - {round(confidence, 2)}% confidence"
 
-# Text input box
-review = st.text_area("Enter your movie review here: ",height=150)
+app = gr.Interface(
+    fn=predict_sentiment,
+    inputs=gr.Textbox(lines=5, placeholder="Enter your movie review here.."),
+    outputs=gr.Textbox(label="Sentiment"),
+    title = "IMDB sentiment Analyzer",
+    description="Type a movie review and find out if it's positive or negative!"
+    
+)
 
-#Button
-if st.button("Analyze Sentiment"):
-    if review.strip() == "":
-        st.warning("Please eneter a review first!")
-    else:
-        cleaned = clean_text(review)
-        vectorized = tfidf.transform([cleaned])
-        prediction = model.predict(vectorized)[0]
-        confidence = max(model.predict_proba(vectorized)[0]) *100
-
-        if prediction == 'positive':
-            st.success(f"POSITIVE - {round(confidence, 2)}% confidence")
-        else:
-            st.error(f"NEGATIVE - {round(confidence, 2)}% confidence")
+app.launch()
